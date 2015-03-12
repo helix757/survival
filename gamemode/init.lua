8b973e9bbac50ba3c5a36f55cb829d8f0491815e
helix757 <jarodmcgovern@yahoo.com>
2015-03-12 14:12:20 -0400

    Sv Modules include
    This will make any file with the sv in the beginning of the name be
    included in the serverside. This is how we will make independent.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,1 +1,10 @@
 include("shared.lua")
+
+local function SvModules()
+	for k,v in pairs(file.Find("survival/gamemode/*.lua", "LUA")) do
+		if string.sub(v,1,2) == "sv" then
+			print("[SV] Loaded serverside module"..v)
+			include(v)
+		end
+	end
+end
