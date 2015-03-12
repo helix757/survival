2efe8160a389e54c42f64c7ab569bb4fad157f39
helix757 <jarodmcgovern@yahoo.com>
2015-03-12 18:24:25 -0400

    Added a better blur.
    @nikorev  Feel free to change anything you need

--- gamemode/cli_hud.lua
+++ gamemode/cli_hud.lua
@@ -1,14 +1,22 @@
-
- print("test.lua")
-
+local ourMat = Material( "blurgm" )
 function survival_hud()
-
+	for i=1, 10 do -- Makes blur more dense
+		ourMat:SetFloat("$blur", Fraction)
+		ourMat:Recompute()
+		surface.SetDrawColor( 255, 255, 255, 255 )
+		surface.SetMaterial( ourMat )
+		render.UpdateFullScreenDepthTexture( )
+		render.UpdatePowerOfTwoTexture( )
+		render.UpdateRefractTexture( )
+		if ( render ) then render.UpdateScreenEffectTexture() end
+		surface.DrawTexturedRect( 15, ScrH() - 495, 350, 200 )
+	end
 draw.RoundedBox(4, 10, ScrH() - 500, 360, 210, Color(85, 0, 0, 255)) -- Outer Box
 draw.RoundedBox(4, 15, ScrH() - 495, 350, 200, Color(170, 57, 57, 255)) -- Inner box
 
-surface.SetDrawColor( Color(170, 57, 57, 255) )
-surface.SetMaterial( Material( "blurgm.vmt" ) )
+surface.SetMaterial( Material( "blurgm" ) )
 surface.DrawTexturedRect( 15, ScrH() - 495, 350, 200 )
+
 
 widthhp =  math.Clamp(LocalPlayer():Health() * 3, 0, 300)
 widthap =  math.Clamp(LocalPlayer():Armor() * 3, 0, 300)
@@ -19,4 +27,3 @@ draw.RoundedBox(0, 185 - widthap/2, ScrH() - 345, math.Clamp(LocalPlayer():Armor
 draw.DrawText(LocalPlayer():GetName(), "DermaLarge", 140, ScrH() - 485, Color(255, 255, 255, 255))
 end
 hook.Add("HUDPaint", "survival_hud", survival_hud)
-
