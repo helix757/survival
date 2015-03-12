c81ac2f02146ef63978252eec513cf73fd6840f4
nikorev <nikorev00@aol.com>
2015-03-12 18:10:15 -0400

    HUD Update

--- gamemode/cli_hud.lua
+++ gamemode/cli_hud.lua
@@ -1,15 +1,33 @@
-local ourMat = Material( "blurgm.vmt" )
+
+ 
+ print("test.lua")
+
+local function ClModules()
+	for k,v in pairs(file.Find("survival/gamemode/*.lua", "LUA")) do
+		if string.sub(v,1,3) == "cli" then
+			print("[CLI] Loaded clientside module"..v)
+			include(v)
+		end
+	end
+end
+hook.Add("Initialize", "Cl_Modules_Hook", ClModules) 
+
 function survival_hud()
 
-draw.RoundedBox(4, 10, ScrH() - 5, 360, 210, Color(85, 0, 0, 255)) -- Outer Box
-draw.RoundedBox(4, 15, ScrH() - 10, 350, 200, Color(170, 57, 57, 255)) -- Inner box
+draw.RoundedBox(4, 10, ScrH() - 500, 360, 210, Color(85, 0, 0, 255)) -- Outer Box
+draw.RoundedBox(4, 15, ScrH() - 495, 350, 200, Color(170, 57, 57, 255)) -- Inner box
 
-draw.RoundedBox(0, 20, ScrH() - 15, math.Clamp(LocalPlayer():Health(), 0, 200), 40, Color(212, 106, 106, 255)) -- Health bar
-draw.RoundedBox(0, 20, ScrH() - 65, math.Clamp(LocalPlayer():Armor(), 0, 200), 40, Color(255, 170, 170, 255)) -- Armor bar
+surface.SetDrawColor( Color(170, 57, 57, 255) )
+surface.SetMaterial( Material( "blurgm.vmt" ) )
+surface.DrawTexturedRect( 15, ScrH() - 495, 350, 200 )
 
-	surface.SetDrawColor( 0, 0, 0, 255 )
-	surface.SetMaterial( ourMat )
-	surface.DrawTexturedRect( 0, 0, 512, 512 )
+widthhp =  math.Clamp(LocalPlayer():Health() * 3, 0, 300)
+widthap =  math.Clamp(LocalPlayer():Armor() * 3, 0, 300)
+
+draw.RoundedBox(0, 185 - widthhp/2, ScrH() - 395, math.Clamp(LocalPlayer():Health() * 3, 5, 300), 40, Color(212, 106, 106, 255)) -- Health bar
+draw.RoundedBox(0, 185 - widthap/2, ScrH() - 345, math.Clamp(LocalPlayer():Armor() * 3, 5, 300), 40, Color(255, 170, 170, 255)) -- Armor bar
+
+draw.DrawText(LocalPlayer():GetName(), "DermaLarge", 140, ScrH() - 485, Color(255, 255, 255, 255))
 end
 hook.Add("HUDPaint", "survival_hud", survival_hud)
- 
+
