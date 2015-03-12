6bbf0b182c5a6046e8b28f62cb7bf48d45527302
helix757 <jarodmcgovern@yahoo.com>
2015-03-12 18:13:04 -0400

    Fixed niko doubling uneeded functions

--- gamemode/cli_hud.lua
+++ gamemode/cli_hud.lua
@@ -1,16 +1,5 @@
 
- 
  print("test.lua")
-
-local function ClModules()
-	for k,v in pairs(file.Find("survival/gamemode/*.lua", "LUA")) do
-		if string.sub(v,1,3) == "cli" then
-			print("[CLI] Loaded clientside module"..v)
-			include(v)
-		end
-	end
-end
-hook.Add("Initialize", "Cl_Modules_Hook", ClModules) 
 
 function survival_hud()
 
