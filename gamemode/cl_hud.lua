541e38ccf505223ba5bb0a12a00ba9854fcc768d
nikorev <nikorev00@aol.com>
2015-03-11 22:44:06 -0400

    Wow this works?
    Damn son. Committing this file to master.

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -7,4 +7,6 @@ draw.RoundedBox(4, 15, ScrH() - 10, 350, 200, Color(170, 57, 57, 255)) -- Inner 
 draw.RoundedBox(0, 20, ScrH() - 15, math.Clamp(LocalPlayer():Health(), 0, 200), 40, Color(212, 106, 106, 255)) -- Health bar
 draw.RoundedBox(0, 20, ScrH() - 65, math.Clamp(LocalPlayer():Armor(), 0, 200), 40, Color(255, 170, 170, 255)) -- Armor bar
 
+-- Testing github for windows
+
 end
