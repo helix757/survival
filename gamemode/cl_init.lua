939499cd7ad094b1625d5a4d49c945a529bc4eac
helix757 <jarodmcgovern@yahoo.com>
2015-03-12 15:31:59 -0400

    Cl Module includes
    Same as SV.

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,1 +1,12 @@
 print("test.lua")
+
+local function ClModules()
+	for k,v in pairs(file.Find("survival/gamemode/*.lua", "LUA")) do
+		if string.sub(v,1,2) == "cl" then
+			print("[SV] Loaded clientside module"..v)
+			AddCSLuaFile(v)
+			include(v)
+		end
+	end
+end
+hook.Add("Initialize", "Cl_Modules_Hook", ClModules) 
