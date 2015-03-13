79bc8758b06a72a03efffb68efd06e054373c481
helix757 <jarodmcgovern@yahoo.com>
2015-03-13 18:02:39 -0400

    Added @BCBEST 's `team.Setup` to the right place. This is only temporary as I am making a custom function

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,7 +2,3 @@
 if SERVER then
   AddCSLuaFile("shared.lua")
 end
-
-team.SetUp( 1, "Hunter", Color( 255, 0, 0 ) )
-team.SetUp( 2, "BowMan", Color( 0, 255, 255 ) )
-team.SetUp( 3, "Prepper", Color( 255, 255, 0 ) )
--- gamemode/sv_classpick.lua
+++ gamemode/sv_classpick.lua
@@ -5,3 +5,8 @@ function OpenDerma(ply)
 	net.Send(ply)
 end
 hook.Add("PlayerInitialSpawn", "Open_Derma", OpenDerma)
+
+
+team.SetUp( 1, "Hunter", Color( 255, 0, 0 ) )
+team.SetUp( 2, "BowMan", Color( 0, 255, 255 ) )
+team.SetUp( 3, "Prepper", Color( 255, 255, 0 ) )
