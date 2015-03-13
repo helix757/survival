d7440df5546340fe18fab3594f7c283d25916405
helix757 <jarodmcgovern@yahoo.com>
2015-03-13 13:59:01 -0400

    Added auto adding shared files. sh_ marks a shared file.

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,6 +1,17 @@
 if SERVER then
   AddCSLuaFile("shared.lua")
 end
+
+local function ShModules()
+	for k,v in pairs(file.Find("gamemode/*.lua", "LUA")) do
+		if string.sub(v,1,2) == "sh" then
+			print("[SV] Loaded shared module "..v)
+			include(v)
+		end
+	end
+end
+hook.Add("Initialize", "Sh_Modules_Hook", ShModules)
+
 team.SetUp( 1, "Hunter", Color( 255, 0, 0 ) )
 team.SetUp( 2, "BowMan", Color( 0, 255, 255 ) )
 team.SetUp( 3, "Prepper", Color( 255, 255, 0 ) )
