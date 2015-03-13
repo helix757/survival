be9b47b9a7eda19347efd199cae3d4f56d301239
helix757 <jarodmcgovern@yahoo.com>
2015-03-13 13:56:37 -0400

    You are using the wrong functions and including files the wrong way.

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,21 +1,6 @@
 if SERVER then
   AddCSLuaFile("shared.lua")
 end
-
-
-AddCSLuaFile( "ClassPick/cli_classpick.lua" )
-AddCSLuaFile( "ClassPick/sv_classpick.lua" )
-include( "ClassPick/cli_classpick.lua" )
-
 team.SetUp( 1, "Hunter", Color( 255, 0, 0 ) )
 team.SetUp( 2, "BowMan", Color( 0, 255, 255 ) )
 team.SetUp( 3, "Prepper", Color( 255, 255, 0 ) )
-
-local Class = {}
-	Class.SetModel( "models/player/police.mdl" )
-	Class.SetMaxHealth = 100
-	Class.SetHealth = 100
-	Class.WalkSpeed = 250
-	Class.RunSpeed = 500
-	Class.TeammateNoCollide = false
-player_manager.RegisterClass( "Default", Class, "player_default" )
