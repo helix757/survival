523b190940ba23226899d6d6c9ccda93ac40c028
helix757 <jarodmcgovern@yahoo.com>
2015-03-13 14:02:40 -0400

    Added Blue messages for console so they really stand out.

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -3,7 +3,7 @@ print("test.lua")
 local function ClModules()
 	for k,v in pairs(file.Find("survival/gamemode/*.lua", "LUA")) do
 		if string.sub(v,1,3) == "cli" then
-			print("[CLI] Loaded clientside module "..v)
+			Error("[CLI] Loaded clientside module "..v)
 			include(v)
 		end
 	end
--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,7 +3,7 @@ include("shared.lua")
 local function SvModules()
 	for k,v in pairs(file.Find("gamemode/*.lua", "LUA")) do
 		if string.sub(v,1,2) == "sv" then
-			print("[SV] Loaded serverside module "..v)
+			Error("[SV] Loaded serverside module "..v)
 			include(v)
 		end
 	end
@@ -13,7 +13,7 @@ hook.Add("Initialize", "Sv_Modules_Hook", SvModules)
 local function ClModules()
 	for k,v in pairs(file.Find("survival/gamemode/*.lua", "LUA")) do
 		if string.sub(v,1,3) == "cli" then
-			print("[CLI] Loaded clientside module "..v)
+			Error("[CLI] Loaded clientside module "..v)
 			AddCSLuaFile("survival/gamemode/"..v)
 		end
 	end
