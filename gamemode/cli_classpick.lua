468c6c7f979d012b6ed35a48574cd7d67d12b052
BCBEST <spdgaming@outlook.com>
2015-03-12 21:18:24 -0400

    Most stable class derma so far.

--- gamemode/cli_classpick.lua
+++ gamemode/cli_classpick.lua
@@ -1,21 +1,26 @@
 
 function ClassDerma()
-local Frame = vgui.Create( "DFrame" )
-	Frame:SetTitle( "Test panel" )
-	Frame:SetSize( 300, 300 )
-	Frame:Center()			
-	Frame:MakePopup()
-	Frame.Paint = function( self, w, h ) -- 'function Frame:Paint( w, h )' works too
-		draw.RoundedBox( 0, 0, 0, w, h, Color( 160, 160, 160, 255 ) ) -- Draw a red box instead of the frame
+local BG = vgui.Create( "DFrame" )
+	BG:SetTitle( "Test panel" )
+	BG:SetSize( 300, 300 )
+	BG:Center()			
+	BG:MakePopup()
+	BG.Paint = function( self, w, h ) 
+		draw.RoundedBox( 0, 0, 0, w, h, Color( 160, 160, 160, 255 ) ) 
 	end
 		
-local DButton = vgui.Create("DButton", Frame)
-	DButton:SetText( "Click me I'm pretty!" )
-	DButton:SetTextColor( Color(255, 255, 255) )
-	DButton:SetPos( 100, 100 )
-	DButton:SetSize( 100, 30 )
-	DButton.Paint = function( self, w, h )
-		draw.RoundedBox(0, 0, 0, w, h, Color(255, 0, 0, 255) ) -- Draw a blue button
+local blah = vgui.Create("DButton")
+	blah:SetParent( BG )
+	blah:SetText( "Click me I'm pretty!" )
+	blah:SetTextColor( Color(255, 255, 255) )
+	blah:SetPos( 100, 100 )
+	blah:SetSize( 100, 30 )
+	blah.Paint = function( self, w, h )
+		draw.RoundedBox(0, 0, 0, w, h, Color(255, 0, 0, 255) ) 
+	end
+	blah.DoClick = function()
+		player_manager.SetPlayerClass( ply, "Default" )
+		player_manager.OnPlayerSpawn( ply )
 	end
 end
 net.Receive("OpenClassDerma", ClassDerma)
