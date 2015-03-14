a2d75849fcad10075a213129a9e3dad2abd3c10a
nikorev <nikorev00@aol.com>
2015-03-14 00:00:50 -0400

    --CHECK DESCRIPTION-- Core Aspects of the Class Selection Screen Are In
    Problems / To-Do List:
    - Get teams into an easy table
    - Make the phrases automatically detect and update
    - Make the playermodel panel automatically detect and update
    - Make the weapons panel automatically detect and update

--- gamemode/cli_classpick.lua
+++ gamemode/cli_classpick.lua
@@ -1,4 +1,5 @@
 
+
 function ClassDerma()
 local BG = vgui.Create( "DFrame" )
 	BG:SetTitle( "Test panel" )
@@ -38,19 +39,44 @@ local basepanel = vgui.Create( "DFrame" )
 	function basepanel.Paint()
 		draw.RoundedBox(0, 0, 0, basepanel:GetWide(), basepanel:GetTall(), Color(85, 0, 0, 255))
 		draw.RoundedBox(0, 5, 5, basepanel:GetWide() - 10, basepanel:GetTall() - 10, Color(170, 57, 57, 255))
+		
+		draw.DrawText("Select a Class", "BiggerBloodImpact", basepanel:GetWide() / 2, 4, Color(0, 0, 0, 255), TEXT_ALIGN_CENTER)
+		
+		for k,v in pairs(teamtable) do
+			draw.DrawText("Class Info Here somehow", "SuperSmallBloodImpact", basepanel:GetWide() - 180, basepanel:GetTall() - 225, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER)
+		end
 	end
 	
-local extrainfolabel = vgui.Create("DLabel", basepanel)
-	extrainfolabel:SetPos( basepanel:GetWide() - 200, basepanel:GetTall() - 100 )
-	extrainfolabel:SetSize( basepanel:GetWide() / 2 + 20, 50 )
-	extrainfolabel:Hide()
-	for k,v in pairs(teamtable) do -- FIX THIS
-		extrainfolabel:SetText(v[4]) -- FIX THIS
-	end -- FIX THIS
+local modelpreviewpanel = vgui.Create("DPanel", basepanel)
+	modelpreviewpanel:SetPos( basepanel:GetWide() - 280, 60 )
+	modelpreviewpanel:SetSize( 200, 300 )
+		
+	function modelpreviewpanel.Paint()
+		draw.RoundedBox(0, 0, 0, modelpreviewpanel:GetWide(), modelpreviewpanel:GetTall(), Color(212, 106, 106, 255))
+	end
+		
+local classwepsandstats = vgui.Create( "DPanel", basepanel )
+	classwepsandstats:SetPos( basepanel:GetWide() - 325, basepanel:GetTall() - 175 )
+	classwepsandstats:SetSize(300, 150)
+	
+	function classwepsandstats.Paint()
+		draw.RoundedBox(0, 0, 0, classwepsandstats:GetWide(), classwepsandstats:GetTall(), Color(212, 106, 106, 255))
+		
+		draw.DrawText("Weapons", "SmallBloodImpact", classwepsandstats:GetWide() / 2, 5, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER)
+	end
+	
+		
+local modelpreview = vgui.Create( "DModelPanel", modelpreviewpanel ) -- Make it retrieve playermodel for a team
+modelpreview:SetPos(5, 5)
+modelpreview:SetSize(modelpreviewpanel:GetWide() - 10, modelpreviewpanel:GetTall() - 10)
+modelpreview:SetModel( "models/player/alyx.mdl" )
+modelpreview:SetCamPos(Vector( 40, 0, 55))
+function modelpreview:LayoutEntity( Entity ) return end
+
 	
 local teamlist = vgui.Create("DPanelList", basepanel)
-	teamlist:SetPos( 0, 60 )
-	teamlist:SetSize( basepanel:GetWide() / 2 - 20, basepanel:GetTall() - 120)
+	teamlist:SetPos( 5, 60 )
+	teamlist:SetSize( basepanel:GetWide() / 2 - 20, basepanel:GetTall() - 65)
 	teamlist:SetSpacing(10)
 	teamlist:SetPadding(5)
 	teamlist:EnableHorizontal( true ) 
@@ -58,6 +84,7 @@ local teamlist = vgui.Create("DPanelList", basepanel)
 	teamlist:MoveToFront()
 	
 	for k,v in pairs(teamtable) do
+	
 		local teampanel = vgui.Create("DPanel", teamlist)
 			teampanel:SetPos( 5, 5 )
 			teampanel:SetSize( teamlist:GetWide(), 40 )
@@ -80,9 +107,8 @@ local teamlist = vgui.Create("DPanelList", basepanel)
 		end
 		
 		function teampanelbutton.DoClick()
-			print("hi")
-			extrainfolabel:Hide()
-			extrainfolabel:Show()
+			print("The team you chose was "..v[1])
+			print("Thats all for now...")
 		end
 	end
 end
@@ -92,6 +118,13 @@ teamtable = {}
 teamtable[1] = {"Hunter", Color(255, 0, 0), "models/weapons/v_357.mdl", "Go in sneaky beaky like."}
 teamtable[2] = {"Bowman", Color(255, 0, 0), "models/weapons/v_357.mdl", "Arch down your foes."}
 teamtable[3] = {"Prepper", Color(255, 0, 0), "models/weapons/v_357.mdl", "Almost as good as Dr. Pepper."}
+
+
+surface.CreateFont("BiggerBloodImpact", {
+	font = "Bloody Impact", 
+	size = 45, 
+	antialias = true,
+})
 
 surface.CreateFont("BigBloodImpact", {
 	font = "Bloody Impact", 
@@ -105,3 +138,8 @@ surface.CreateFont("SmallBloodImpact", {
 	antialias = true,
 })
 
+surface.CreateFont("SuperSmallBloodImpact", {
+	font = "Bloody Impact", 
+	size = 20, 
+	antialias = true,
+})
