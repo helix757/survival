c306c1c67e4b420bf7335104d612df39f0d897fc
nikorev <nikorev00@aol.com>
2015-03-29 14:25:06 -0400

    Made messages look sexy

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,7 +3,7 @@ include("shared.lua")
 function svModules()
 	for k,v in pairs(file.Find("survival/gamemode/*.lua", "LUA")) do
 		if string.sub(v,1,2) == "sv" then
-			MsgC( Color(255, 0, 0), "[Survival]", Color(255, 255, 255), ": Serverside modules loaded.\n")
+			MsgC( Color(255, 0, 0), "[Survival]", Color(255, 255, 255), ": Loaded serverside file "..v.."\n")
 			include("survival/gamemode/"..v)
 		end
 	end
@@ -13,7 +13,7 @@ svModules()
 local function AddClModules()
 	for k,v in pairs(file.Find("survival/gamemode/*.lua", "LUA")) do
 		if string.sub(v,1,3) == "cli" then
-			MsgC( Color(255, 0, 0), "[Survival]", Color(255, 255, 255), ": Clientside modules loaded.\n")
+			MsgC( Color(255, 0, 0), "[Survival]", Color(255, 255, 255), ": Loaded clientside file "..v.."\n")
 			AddCSLuaFile("survival/gamemode/"..v)
 		end
 	end
@@ -24,7 +24,7 @@ hook.Add("Initialize", "Cl_Modules_Hook", AddClModules)
 local function AddShModules()
 	for k,v in pairs(file.Find("survival/gamemode/*.lua", "LUA")) do
 		if string.sub(v,1,2) == "sh" then 
-			MsgC( Color(255, 0, 0), "[Survival]", Color(255, 255, 255), ": Shared modules loaded.\n")
+			MsgC( Color(255, 0, 0), "[Survival]", Color(255, 255, 255), ": Loaded shared file "..v.."\n")
 			AddCSLuaFile(v)
 			include(v)
 		end
